babf4bb004d2d5aee1ecee2694a8dade8ec22f8a
MaxMaximov123 <99262907+MaxMaximov123@users.noreply.github.com>
2022-12-26 23:57:17 +0300

    Сделал снег

--- Games/Main_window.py
+++ Games/Main_window.py
@@ -1,8 +1,44 @@
+import random
+
 import pygame as pg
+
+from Games import config
 from Games.Galaga_objects.Cursor import Cursor
 from Games.Galaga_objects.Galaga import Galaga
-from Games import config
 from Games.Tanks_objects.Tanks import Tanks
+
+
+class Snow(pg.sprite.Sprite):
+	def __init__(self, game, f=True):
+		super().__init__(game.snow_groups)
+		color = pg.Color(255, 255, 255, random.randint(100, 200))
+		self.game = game
+		self.radius = random.randint(1, 8)
+		self.image = pg.Surface((2 * self.radius, 2 * self.radius),
+								pg.SRCALPHA, 32)
+		pg.draw.circle(
+			self.image, color,
+			(self.radius, self.radius), self.radius)
+		if f:
+			self.x, self.y = random.randint(-20, config.WIDTH), random.randint(-20, config.HEIGHT)
+		else:
+			if random.randint(0, 1):
+				self.x, self.y = random.randint(-20, config.WIDTH), random.randint(-20, 20)
+			else:
+				self.x, self.y = random.randint(-20, 20), random.randint(-20, config.HEIGHT)
+		self.rect = pg.Rect(self.x, self.y, 2 * self.radius, 2 * self.radius)
+		self.vector_x = random.randint(1, 5)
+		self.vector_y = random.randrange(1, 5)
+		self.speed = random.randint(10, 20)
+		self.x, self.y = self.rect.x, self.rect.y
+
+	def update(self):
+		self.x += self.vector_x * self.speed / config.FPS
+		self.y += self.vector_y * self.speed / config.FPS
+		self.rect.x, self.rect.y = self.x, self.y
+		if self.x > config.WIDTH or self.y > config.HEIGHT:
+			self.kill()
+			Snow(self.game, False)
 
 
 class MainWindow:
@@ -19,6 +55,8 @@ class MainWindow:
 		self.headers_games = []
 		self.correct_game = None
 		self.correct_position_ind = None
+		self.snow_groups = pg.sprite.Group()
+		self.snow_count = random.randint(300, 800)
 
 	def run(self):
 		pg.init()
@@ -29,9 +67,10 @@ class MainWindow:
 		cursor = Cursor(self.screen)
 		self.draw(config.GAMES_NAMES)
 		self.correct_position_ind = 0
+		while len(self.snow_groups) < self.snow_count:
+				Snow(self)
 		while running:
 			self.screen.fill('black')
-			self.draw(config.GAMES_NAMES)
 			cursor.update_pos(self.games_names_pos[self.correct_position_ind % len(self.games_names_pos)])
 			for event in pg.event.get():
 				if event.type == pg.QUIT:
@@ -45,7 +84,7 @@ class MainWindow:
 						self.active_game = self.games[self.correct_position_ind % len(self.games)](self)
 						self.active_game.run()
 						self.correct_game = self.correct_position_ind
-						# self.screen.fill('black')
+					# self.screen.fill('black')
 
 				if event.type == pg.MOUSEBUTTONDOWN:
 					if event.button == 1:
@@ -58,22 +97,26 @@ class MainWindow:
 						self.correct_position_ind -= 1
 					if event.button == 5:
 						self.correct_position_ind += 1
-
+			self.snow_groups.update()
+			self.snow_groups.draw(self.screen)
+			self.draw(config.GAMES_NAMES)
 			clock.tick(config.FPS)
 			pg.display.flip()
 		pg.quit()
 
 	def draw(self, games_names):
-		self.screen.fill((0, 0, 0))
+		# self.screen.fill((0, 0, 0))
 		font = pg.font.SysFont('monospace', self.head_text_size)
-		text = font.render("Выберите игру", True, ('white'))
+		font.bold = True
+		text = font.render("Выберите игру", True, ('#ffcc00'))
 		text_x = config.WIDTH // 2 - text.get_width() // 2
 		text_y = config.HEIGHT // 5 - text.get_height() // 2
 		self.screen.blit(text, (text_x, text_y))
 		self.headers_games = []
 		for h, name in enumerate(games_names):
 			font = pg.font.SysFont("monospace", self.game_text_size)
-			text = font.render(name, True, ('white'))
+			font.bold = True
+			text = font.render(name, True, ('#ffcc00'))
 			text_x = config.WIDTH // 2 - text.get_width() // 2
 			text_y = config.HEIGHT // 15 * (h + 5) - text.get_height() // 2
 			self.screen.blit(text, (text_x, text_y))
